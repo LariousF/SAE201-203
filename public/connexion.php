607b6fbb73a6e0d8ea9d640719e3a030df40e45f
RaphouCode <b.raphaelmail@gmail.com>
2025-05-20 16:26:07 +0200

    Resolution de conflits final pitié

--- public/connexion.php
+++ public/connexion.php
@@ -1,8 +1,6 @@
-
-<?php
-require_once 'src/config/db_connect.php';
-require_once 'src/model/authentification.php';
+<?php 
 require_once '../src/config/db_connect.php';
+require_once '../src/model/authentification.php';
 
 session_start();
 
